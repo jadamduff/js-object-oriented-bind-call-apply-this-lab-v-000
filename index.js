f8912bbfd53022d8dbd4e9be48b9880b42aa177e
Adam Duff <j.adam.duff@gmail.com>
2018-10-26 06:40:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ function setThisWithCall(fn, thisValue, args) {
   return fn.call(thisValue, args);
 }
 
-unction setThisWithCApply(fn, thisValue, args) {
+unction setThisWithApply(fn, thisValue, args) {
   return fn.apply(thisValue, args);
 }
